4eb495d051e90c668f84cb08811c80c5f12d99e9
simopunkc <pembela.allah@gmail.com>
2022-06-10 06:23:15 +0700

    submit challenge 7 oleh budiyanto

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const data1 = await promiseTheaterIXX()
+  const data2 = await promiseTheaterVGC()
+  let count = 0
+  data1.concat(data2).forEach(element => {
+    if (element.hasil == emosi) {
+      count += 1
+    }
+  });
+  return count
+};
 
 module.exports = {
   promiseOutput,
